fa302606fee1b0644980d80e3c126579c747bcd6
Stanislav Blokh <s.e.blokh@gmail.com>
2017-01-04 13:33:22 +0200

    add "start" command

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bot = new TelegramBot(token, {
 
 var renameJson = {};
 var URL = 'http://rename.dp.ua/rename.json'; // all constants should be in CAPS
+var MINRESPONCELENGTH = 3;
+
 
 request.get({
 	url: URL,
@@ -31,30 +33,33 @@ bot.on('message', function (msg) {
 
 	var chatResponce = msg.text.toLowerCase();
 	var chatId = msg.chat.id;
-	//var arr = renameJson; dont double variables!!!
-	var flag = 0, objects;
+	var flag = 0,objects;
 	
-	console.log(msg);
 
-	if (chatResponce.length < 3) { // 3 - is the "magic number", this is antipattern
-		bot.sendMessage(chatId, ("Додайте ще букв, будь ласка"))
+	if (chatResponce === "/start") {
+		bot.sendMessage(chatId, "Вiдправте стару назву, або кiлька букв для пошуку");
 	} else {
-		for (var key in  renameJson) {
-			if (key !== "lastUpdate") {
-				objects = renameJson[key].objects;
-				for (var i = 0; i < objects.length; i++) {
-					if ( objects[i].oldName.toLowerCase().indexOf(chatResponce) !== -1) {
-					// what about collect results to one object and send it all after full search?
-						bot.sendMessage(chatId, ("Стара назва: " + objects[i].oldName + " \n" + "Нова назва: " + objects[i].newName), {
-							caption: "I'm a bot!"
-						});
-						flag++;
-					}
-				};
+
+		if (chatResponce.length < MINRESPONCELENGTH) { 
+			bot.sendMessage(chatId, ("Додайте ще букв, будь ласка"))
+		} else {
+			for (var key in renameJson) {
+				if (key !== "lastUpdate") {
+					objects = renameJson[key].objects;
+					for (var i = 0; i < objects.length; i++) {
+						if (objects[i].oldName.toLowerCase().indexOf(chatResponce) !== -1) {
+							// what about collect results to one object and send it all after full search?
+							bot.sendMessage(chatId, ("Стара назва: " + objects[i].oldName + " \n" + "Нова назва: " + objects[i].newName), {
+								caption: "I'm a bot!"
+							});
+							flag++;
+						}
+					};
+				}
 			}
-		}
-		if (flag === 0) {
-			bot.sendMessage(chatId, ("Нажаль, нiчого не змогли знайти (("))
+			if (flag === 0) {
+				bot.sendMessage(chatId, ("Нажаль, нiчого не змогли знайти (("))
+			}
 		}
 	}
 });
